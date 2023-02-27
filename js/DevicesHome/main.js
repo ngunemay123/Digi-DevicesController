7a95b47624fe71c0118f1eb77ca64a028a1223e4
Đinh Quốc Cường <99747977+Tr6r@users.noreply.github.com>
2023-02-27 21:59:09 +0700

    27-2

--- js/DevicesHome/main.js
+++ js/DevicesHome/main.js
@@ -1,15 +1,16 @@
 const URL = "https://digi27.azurewebsites.net/api/healthies";
 var email;
 var PIDc = [];
-var check_parameter = [" "," "," "," "," "," "," "];
-var bien = [" "," "," "," "," "," "," "];
-var cot2 = [];
-var count;
+var bang = [];
+var count_bold = 0;
+var count = 0;
+var test ;
 var bar = ["ID","Device","Location","Date","Time","Parameter","Details"];
+
 async function check()
 {		                        
-    email = sessionStorage.getItem('email');
-    await axios.get(URL + "/SearchByEmail/"+email).then((response) =>{
+    // email = sessionStorage.getItem('email');
+    await axios.get(URL + "/SearchByEmail/tr6r20@gmail,com").then((response) =>{
         var healthies = response.data;
 
         for(var human of healthies )
@@ -37,7 +38,9 @@ async function GetParameter()
     await axios.get(URL + "/SearchByUIDOwner/"+sessionStorage.getItem('UID')).then((response) =>{
         var healthies = response.data;
         var i =0;
+        test = 0;
         count = 0;
+        bang.push(bar);
         for(var human of healthies )
         {
             PIDc[i] =  human.PID;
@@ -47,11 +50,9 @@ async function GetParameter()
     // setTimeout(async () => {
         for(var j = (PIDc.length - 1);j >= 0 ;)
         {
-            check_parameter.length = 0;
-            bien.length = 0;
-            cot2.length = 0;
-            check_parameter = [" "," "," "," "," "," "," "];
-            bien = [" "," "," "," "," "," "," "];
+            var bien_parameter = [];
+            var check_parameter = [" "," "," "," "," "," "," "];
+            var bien = [" "," "," "," "," "," "," "];
             j  = j -1 ;
             await axios.get(URL + "/SearchByPIDProduct/"+PIDc[j+1]).then(async (response) =>{
                 var healthies = response.data;   
@@ -61,7 +62,7 @@ async function GetParameter()
                     {
                         if(PIDc[j+1] === human.PID )
                         {
-                            
+                            test = test + 1;
                             break;
                         }
                     }
@@ -70,35 +71,35 @@ async function GetParameter()
                     //    //đọc trực tiếp       
                             if(PIDc[j+1] === human.PID)
                             {
-                                
+                                test = test + 1;
                                 // alert(PIDc[j+1] + human.PID + "N");
                                 if(human.Acceleration === "Y")
                                 {
-                                    check_parameter.unshift("Acceleration");
+                                    check_parameter.push("Acceleration");
                                 }
                                 if(human.Speed === "Y")
                                 {
-                                    check_parameter.unshift("Speed");
+                                    check_parameter.push("Speed");
                                 }
                                 if(human.Temperature === "Y")
                                 {
-                                    check_parameter.unshift("Temperature");
+                                    check_parameter.push("Temperature");
                                 }
                                 if(human.Humidity === "Y")
                                 {
-                                    check_parameter.unshift("Humidity");
+                                    check_parameter.push("Humidity");
                                 }
                                 if(human.Pressure === "Y")
                                 {
-                                    check_parameter.unshift("Pressure");
+                                    check_parameter.push("Pressure");
                                 }
                                 if(human.Speed_Of_Winds === "Y")
                                 {
-                                    check_parameter.unshift("Speed_Of_Winds");
+                                    check_parameter.push("Speed_Of_Winds");
                                 }
                                 if(human.Wind_Direction === "Y")
                                 {
-                                    check_parameter.unshift("Wind_Direction");
+                                    check_parameter.push("Wind_Direction");
                                 }
                                 
                                 await axios.get(URL + "/SearchByPIDParameterDR/"+PIDc[j+1]).then((response) =>{
@@ -113,11 +114,11 @@ async function GetParameter()
                                         // let slip_Current_Time_date = slip_Current_Time[0].split("/");
                                         // let slip_Current_Time_time = slip_Current_Time[1].split(":");
                                         
-                                        cot2.push(human.PID);
-                                        cot2.push(human.PName);
-                                        cot2.push("HCM");
-                                        cot2.push(slip_Current_Time[0]);
-                                        cot2.push(slip_Current_Time[1]+" "+slip_Current_Time[2]);
+                                        bien_parameter.push(human.PID);
+                                        bien_parameter.push(human.PName);
+                                        bien_parameter.push("HCM");
+                                        bien_parameter.push(slip_Current_Time[0]);
+                                        bien_parameter.push(slip_Current_Time[1]+" "+slip_Current_Time[2]);
                                         
                                         // if(slip_Current_Time_date[0] === "6" 
                                         //     && slip_Current_Time_date[1] === "12"
@@ -125,37 +126,37 @@ async function GetParameter()
                                         // {
                                             for(var i = 0;i < check_parameter.length;i++)
                                             {
-                                                if(check_parameter[i] === "Wind_Direction")
+                              
+                                                if(check_parameter[i] === "Acceleration")
                                                 {
-                                                    bien.unshift(human1.Wind_Direction);
-                                                }
-                                                else if(check_parameter[i] === "Speed_Of_Winds")
+                                                    bien.push(human1.Acceleration);
+                                                } 
+                                                else if(check_parameter[i] === "Speed")
                                                 {
-                                                    bien.unshift(human1.Speed_Of_Winds);
-                                                }
-                                                else if(check_parameter[i] === "Pressure")
-                                                {
-                                                    bien.unshift(human1.Pressure);
-                                                }
-                                                else if(check_parameter[i] === "Humidity")
-                                                {
-                                                    bien.unshift(human1.Humidity + "°F");
+                                                    bien.push(human1.Speed+"m/s");
                                                 }
                                                 else if(check_parameter[i] === "Temperature")
                                                 {
-                                                    bien.unshift(human1.Temperature + "°C");
+                                                    bien.push(human1.Temperature + "°C");
                                                 }
-                                                else if(check_parameter[i] === "Speed")
+                                                else if(check_parameter[i] === "Humidity")
                                                 {
-                                                    bien.unshift(human1.Speed+"m/s");
+                                                    bien.push(human1.Humidity + "°F");
                                                 }
-                                                else if(check_parameter[i] === "Acceleration")
+                                                else if(check_parameter[i] === "Pressure")
                                                 {
-                                                    bien.unshift(human1.Acceleration);
-                                                }  
+                                                    bien.push(human1.Pressure);
+                                                }
+                                                else if(check_parameter[i] === "Speed_Of_Winds")
+                                                {
+                                                    bien.push(human1.Speed_Of_Winds);
+                                                }
+                                                else if(check_parameter[i] === "Wind_Direction")
+                                                {
+                                                    bien.push(human1.Wind_Direction);
+                                                }
                                             }
-                                            cot2.push(bien[0]);
-                                            cot2.push("nut");
+                                            
                                             break;
                                         // }
                                     }
@@ -164,83 +165,113 @@ async function GetParameter()
                         }
                     }
                     //code here
-                    MakeTable();
+                    bien.reverse();
+                    check_parameter.reverse();
+                    bien_parameter.push(bien[0]);
+                    bien_parameter.push("nut");
+                    
+                    bang.push(bien_parameter);
+                    bang.push(check_parameter);
+                    bang.push(bien);
+                    if (test === PIDc.length - 1)
+                    {
+                        MakeTable();
+                    }
                 }
+                
             })
+            
         }
+        
 }
 function MakeTable()
 {
-    bar.push(check_parameter[0]);
-    bar.push("Detail");
-    
     var container = document.getElementById("cuong");
-    var countRow = 4;
+    var countRow = bang.length;
     var countColumn = 7;
     var tagTable = document.createElement("table");
+    
+    
     tagTable.style.border = 1;
     // tagTable.style.width = "200px";
     // tagTable.style.height = "3px" ;
    
     tagTable.style.textAlign = "center";
-
+   
     for (var i = 0; i < countRow; i++) {
+        
         var tagRow = document.createElement("tr");
         tagRow.style.width = "fix-content";
         tagRow.style.fontSize = "16px";
         tagTable.appendChild(tagRow);
         
         for(var j = 0; j < countColumn; j++) {
-          var tagColumn = document.createElement("td");
+        var tagColumn = document.createElement("td");
           tagColumn.style.width = "fix-content";
             tagColumn.style.height = "3px";
             tagColumn.style.fontSize = "16px";
-          if(i === 0 && count < 7 )
+            tagColumn.style.backgroundColor = 'lightgray';
+            var textNode = document.createTextNode(bang[i][j]);
+            tagColumn.appendChild(textNode);
+            tagRow.appendChild(tagColumn);
+            count = count + 1 ;
+            count_bold = count_bold + 1;
+          if(i === 0 )
           {
-            count ++;
-            var textNode = document.createTextNode(bar[j]);
             tagColumn.style.color = 'white';
             tagColumn.style.backgroundColor = 'black';
-            tagColumn.appendChild(textNode);
-            tagRow.appendChild(tagColumn);
+            count = 0;
+            count_bold = 0;
           }
-          else if(i === 1)
+          if (count >= 8 && count <= 21 )
           {
-            var textNode = document.createTextNode(cot2[j]);
-            tagColumn.style.color = 'black';
-            tagColumn.style.backgroundColor = 'lightgray';
-            tagColumn.appendChild(textNode);
-            tagRow.appendChild(tagColumn);
-          }
-          else if(i === 2)
-          {
-            var textNode = document.createTextNode(check_parameter[j]);
-            tagColumn.style.color = 'black';
             tagColumn.style.backgroundColor = 'white';
-            
-                tagColumn.appendChild(textNode);
-                tagRow.appendChild(tagColumn);
-            
-            
-          }
-          else if(i === 3)
-          {
-            var textNode = document.createTextNode(bien[j]);
             tagColumn.style.color = 'black';
-            tagColumn.style.backgroundColor = 'white';
-            if(bien[j] === "")
+            if(count === 21)
             {
-                break;
+                count = 0;
             }
-            tagColumn.appendChild(textNode);
-            tagRow.appendChild(tagColumn);
           }
+          if(count_bold >= 8 && count_bold <= 14)
+          {
+            tagColumn.style.fontWeight = 'bold';
+          }
+          if(count_bold === 21)
+            {
+                count_bold = 0;
+            }
+        //   else if(i === 1)
+        //   {
+        //     var textNode = document.createTextNode(bien_parameter[j]);
+        //     tagColumn.style.color = 'black';
+        //     tagColumn.style.backgroundColor = 'lightgray';
+        //     tagColumn.appendChild(textNode);
+        //     tagRow.appendChild(tagColumn);
+        //   }
+        //   else if(i === 2)
+        //   {
+        //     var textNode = document.createTextNode(check_parameter[j]);
+        //     tagColumn.style.color = 'black';
+        //     tagColumn.style.backgroundColor = 'white';
+            
+        //         tagColumn.appendChild(textNode);
+        //         tagRow.appendChild(tagColumn);
+        //   }
+        //   else if(i === 3)
+        //   {
+        //     var textNode = document.createTextNode(bien[j]);
+        //     tagColumn.style.color = 'black';
+        //     tagColumn.style.backgroundColor = 'white';
+        //     if(bien[j] === "")
+        //     {
+        //         break;
+        //     }
+        //     tagColumn.appendChild(textNode);
+        //     tagRow.appendChild(tagColumn);
+        //   }
         }
     }
+        
     container.appendChild(tagTable);
-}
-function ResetArr()
-{
-    
 }
 //  setInterval(GetParameter, 10000);
